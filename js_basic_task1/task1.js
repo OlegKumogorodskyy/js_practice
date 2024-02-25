b9bf57cb6a0cea7b8def8d5fe9cd80e5d61d93b0
Oleg Kumogorodskyy <kyym13@gmail.com>
2024-02-25 20:31:32 +0100

    added_task1task2_in_hw3

---
+++ js_basic_task1/task1.js
@@ -0,0 +1,5 @@
+import chalk from 'chalk';
+
+console.log(chalk.red('Textred'));
+console.log(chalk.green('Textgreen'));
+console.log(chalk.blue('Textblue'))
---
+++ js_basic_task1/task2.js
@@ -0,0 +1,7 @@
+const myString = 'String1';
+const myNumber = 42;
+const myBoolean = true;
+
+console.log(myString);
+console.log(myNumber);
+console.log(myBoolean);
