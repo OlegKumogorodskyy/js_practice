ad250d5f937b5808d5cc14b149b25fce68900952
Oleg Kumogorodskyy <kyym13@gmail.com>
2024-02-28 23:45:19 +0100

    delete_js_basic_task1

--- js_basic_task1/task1.js
+++
@@ -1,5 +0,0 @@
-import chalk from 'chalk';
-
-console.log(chalk.red('Textred'));
-console.log(chalk.green('Textgreen'));
-console.log(chalk.blue('Textblue'));
--- js_basic_task1/task2.js
+++
@@ -1,7 +0,0 @@
-const myString = 'String1';
-const myNumber = 42;
-const myBoolean = true;
-
-console.log(myString);
-console.log(myNumber);
-console.log(myBoolean);
